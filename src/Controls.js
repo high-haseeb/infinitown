df04d3d67c6f1c8a13e5585a6c40717bb59e68ec
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-11-22 23:51:57 +0500

    removed rotation and zoom controls

--- src/Controls.js
+++ src/Controls.js
@@ -6,8 +6,8 @@ class Controls{
         this.controls = new MapControls(camera, renderer.domElement);
         this.controls.enableDamping = true;
         this.controls.dampingFactor = 0.1;
-        // this.controls.enableRotate = false;
-        // this.controls.enableZoom = false;
+        this.controls.enableRotate = false;
+        this.controls.enableZoom = false;
     }
     update() {
         this.controls.update();
